120fa9564e85e86017ccf4f4cee66e62609ae9a8
domingosparaiso <domingos.paraiso@gmail.com>
2023-12-08 09:30:43 -0300

    Ajustes na inicialização

--- setup.sh
+++ setup.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
+chmod +x *.sh
 apt update
 apt upgrade
 apt install git make cmake gcc automake flex bison texinfo help2man gawk libtool libtool-bin libncurses-dev libc++-dev g++ bzip2 xz-utils unzip bc rsync cpio gperf zlib1g zlib1g-dev wget
 
-mkdir -p /opt/esp32/scritps
+mkdir -p /opt/esp32
 chown -R esp32:esp32 /opt/esp32
 
-cp install-*.sh /opt/esp32/scritps
-cd /opt/esp32/scritps
-
-/opt/esp32/scripts/install-python.sh
-/opt/esp32/scripts/install-autoconf.sh
+./install-python.sh
+./install-autoconf.sh
 export PATH="$PATH:/opt/esp32/autoconf-2.71/root/bin"
-/opt/esp32/scripts/install-dynconfig.sh
+./install-dynconfig.sh
 export XTENSA_GNU_CONFIG="/opt/esp32/xtensa-dynconfig/esp32s3.so"
 # ct-ng cannot run as root, we'll just do everything else as a user
-sudo esp32 /opt/esp32/scripts/install-toolchain.sh
-/opt/esp32/scripts/install-bootloader.sh
-/opt/esp32/scripts/install-kernel.sh
+sudo esp32 ./install-toolchain.sh
+./install-bootloader.sh
+./install-kernel.sh
 
 #
 # flash
